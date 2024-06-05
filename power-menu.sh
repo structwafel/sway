ffab33773febd8d4089f15ba27ed257bde4728b8
LGXerxes <ditisluca@yahoo.it>
2024-06-05 20:06:27 +0200

    some things

--- power-menu.sh
+++ power-menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | fuzzel -i --dmenu | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout\n Hibernate" | fuzzel -i --dmenu | awk '{print tolower($2)}' )
 
 case $op in 
         poweroff)
@@ -8,31 +8,14 @@ case $op in
         reboot)
                 ;&
         suspend)
+                ;&
+        hibernate)
                 systemctl $op
                 ;;
         lock)
-		swaylock
+        swaylock
                 ;;
         logout)
                 swaymsg exit
                 ;;
 esac
-
-# options="Power Off\nRestart\nSleep\nLog Out"
-
-# chosen=$(echo -e $options | wofi --dmenu --lines 4)
-
-# case $chosen in
-#     'Power Off')
-#         systemctl poweroff
-#         ;;
-#     'Restart')
-#         systemctl reboot
-#         ;;
-#     'Sleep')
-#         systemctl suspend
-#         ;;
-#     'Log Out')
-#         swaymsg exit
-#         ;;
-# esac
